e3bcedb9638fa79ef248e8049714607406b22bf4
victory <vickywane@gmail.com>
2020-10-13 05:50:52 +0100

    testing SM

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ exports.firestoreFunction = function (req, res) {
 // let res = { name: "", email: "", type: "" };
 //res.set("Access-Control-Allow-Origin", "*");
 //const { name, email, type } = req.body;
-const firestore = new Firestore();
-const type = "";
-const document = firestore.collection("users");
-console.log(document);
+// const firestore = new Firestore();
+// const type = "";
+// const document = firestore.collection("users");
+// console.log(document);
 
 // client.accessSecretVersion
 
-//   client
-//     .accessSecretVersion({
-//         name: "firestore-config",
-//     })
-//     .then((res) => {
-//         console.log(res, "sec value");
-//     })
-//     .catch((e) => console.log("error" , e));
+  client
+    .accessSecretVersion({
+        name: "firestore-config",
+    })
+    .then((res) => {
+        console.log(res, "sec value");
+    })
+    .catch((e) => console.log("error" , e));
 
 if (!type) {
     //res.status(422).send("An action type was not specified");
