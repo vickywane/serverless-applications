f0a4efc29f074fe79b1a4b7452f03c5896fa8965
victory <vickywane@gmail.com>
2020-10-13 09:48:51 +0100

    update

--- index.js
+++ index.js
@@ -7,45 +7,52 @@ const client = new SecretManagerServiceClient();
 //console.log(client);
 
 exports.firestoreFunction = function (req, res) {
-// let req = { name: "", email: "", type: "" };
-// let res = { name: "", email: "", type: "" };
-//res.set("Access-Control-Allow-Origin", "*");
-//const { name, email, type } = req.body;
-// const firestore = new Firestore();
-// const type = "";
-// const document = firestore.collection("users");
-// console.log(document);
+    //   let req = { name: "", email: "", type: "" };
+    //   let res = { name: "", email: "", type: "" };
+    //res.set("Access-Control-Allow-Origin", "*");
+    const { name, email, type } = req.body;
 
-// client.accessSecretVersion
+    console.log(req.body);
 
-  client
-    .accessSecretVersion({
-        name: "firestore-config",
-    })
-    .then((res) => {
-        console.log(res, "sec value");
-    })
-    .catch((e) => console.log("error" , e));
+    const firestore = new Firestore({
+        keyFilename: path.join(__dirname, "./service-key.json"),
+    });
+    //  const type = "";
+    const document = firestore.collection("users");
 
-// if (!type) {
-//     //res.status(422).send("An action type was not specified");
-// }
+    // const [file] = client
+    //     .accessSecretVersion({
+    //         name: "firestore-config",
+    //     })
+    //     .then((res) => {
+    //         console.log(res, "sec value");
+    //     })
+    //     .catch((e) => console.log("error"));
 
-// switch (type) {
-//     case "CREATE":
-//         document.add({
-//             //          username: name,
-//             //        email: email,
-//         });
+    if (!type) {
+        res.status(422).send("An action type was not specified");
+    }
 
-//     case "READ":
-//         break;
+    switch (type) {
+        case "CREATE":
+            document
+                .add({
+                    username: name,
+                    email: email,
+                })
+                .then((response) => res.status(200).send(response))
+                .catch((e) =>
+                    res.status(501).send(`error inserting data : ${e}`)
+                );
 
-//     case "GET":
-//         break;
-//     case "UPDATE":
-//         break;
-//     default:
-//         break;
-// }
+        case "READ":
+            break;
+
+        case "GET":
+            break;
+        case "UPDATE":
+            break;
+        default:
+            break;
+    }
 };
