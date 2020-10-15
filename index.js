c6f483507dfafdfe9808cb5ac4d4779f1fa4141e
victory <vickywane@gmail.com>
2020-10-15 05:15:37 +0100

    update

--- index.js
+++ index.js
@@ -2,57 +2,67 @@ require("dotenv").config();
 const { Firestore } = require("@google-cloud/firestore");
 const path = require("path");
 const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+const cors = require("cors")({ origin: true });
 
 const client = new SecretManagerServiceClient();
 //console.log(client);
 
 exports.firestoreFunction = function (req, res) {
-    //   let req = { name: "", email: "", type: "" };
-    //   let res = { name: "", email: "", type: "" };
-    //res.set("Access-Control-Allow-Origin", "*");
-    const { name, email, type } = req.body;
+    //   let req = { email: "", email: "", type: "" };
+    //   let res = { email: "", email: "", type: "" };
+    //res.header("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //   "Access-Control-Allow-Origin",
+    //  "Origin, X-Requested-With, Content-Type, Accept"
+    // );
+    //
+    console.log("invoke");
+    return cors(req, res, () => {
+        const { email, password, type } = req.body;
 
-    console.log(req.body);
+        console.log(email , password, req.body)
+        //console.log(req.body, "data");
+        
+        const firestore = new Firestore({
+            keyFilename: path.join(__dirname, "./service-key.json"),
+        });
+        //  const type = "";
+        const document = firestore.collection("users");
 
-    const firestore = new Firestore({
-        keyFilename: path.join(__dirname, "./service-key.json"),
+        // const [file] = client
+        //     .accessSecretVersion({
+        //         email: "firestore-config",
+        //     })
+        //     .then((res) => {
+        //         console.log(res, "sec value");
+        //     })
+        //     .catch((e) => console.log("error"));
+
+        if (!type) {
+            res.status(422).send("An action type was not specified");
+        }
+
+        switch (type) {
+            case "CREATE":
+                document
+                    .add({
+                        email: email,
+                        password: password,
+                    })
+                    .then((response) => res.status(200).send(response))
+                    .catch((e) =>
+                        res.status(501).send(`error inserting data : ${e}`)
+                    );
+
+            case "READ":
+                break;
+
+            case "GET":
+                break;
+            case "UPDATE":
+                break;
+            default:
+                res.status(422).send(`${type} is not a valid function action`)
+        }
     });
-    //  const type = "";
-    const document = firestore.collection("users");
-
-    // const [file] = client
-    //     .accessSecretVersion({
-    //         name: "firestore-config",
-    //     })
-    //     .then((res) => {
-    //         console.log(res, "sec value");
-    //     })
-    //     .catch((e) => console.log("error"));
-
-    if (!type) {
-        res.status(422).send("An action type was not specified");
-    }
-
-    switch (type) {
-        case "CREATE":
-            document
-                .add({
-                    username: name,
-                    email: email,
-                })
-                .then((response) => res.status(200).send(response))
-                .catch((e) =>
-                    res.status(501).send(`error inserting data : ${e}`)
-                );
-
-        case "READ":
-            break;
-
-        case "GET":
-            break;
-        case "UPDATE":
-            break;
-        default:
-            break;
-    }
 };
