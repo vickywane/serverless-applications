9af7f389d0ccb1c44e2977b286096d977ee1edcf
victory <vickywane@gmail.com>
2020-10-24 15:46:07 +0100

    split functions

--- email/index.js
+++ email/index.js
@@ -7,48 +7,48 @@ const username = process.env.SMTP_USERNAME;
 const password = process.env.SMTP_PASSWORD;
 
 exports.Emailer = function (req, res) {
-  // let sender = process.env.SENDER;
-  // let reciever = req.body.email;
-  // let type = req.body.type;
-  // let token = req.body.token;
-  // let user = req.body.name;
-  // var transport = nodemailer.createTransport({
-  //   host: process.env.HOST,
-  //   port: 587,
-  //   secure: false,
-  //   auth: {
-  //     user: username,
-  //     pass: password,
-  //   },
-  // });
-  // transport.verify(function (error, success) {
-  //   if (error) {
-  //     res
-  //       .status(401)
-  //       .send({ error: `failed to connect with stmp. check credentials` });
-  //   } else {
-  //     res.status(200).send();
-  //   }
-  // });
-  // if (reciever == null) {
-  //   // console.log(req.body)
-  //   res.status(401).send({ error: `Empty email address` });
-  // } else {
-  //   return transport.sendMail(
-  //     {
-  //       from: sender,
-  //       to: reciever,
-  //       subject: "Welcome to GetBeta",
-  //       html: { path: "dist/welcome.html" },
-  //     },
-  //     (error, info) => {
-  //       if (error) {
-  //         console.log(error);
-  //       } else {
-  //         console.log("message sent");
-  //       }
-  //       transport.close();
-  //     }
-  //   );
-  // }
+  let sender = process.env.SENDER;
+  let reciever = req.body.email;
+  let type = req.body.type;
+  let token = req.body.token;
+  let user = req.body.name;
+  var transport = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: username,
+      pass: password,
+    },
+  });
+  transport.verify(function (error, success) {
+    if (error) {
+      res
+        .status(401)
+        .send({ error: `failed to connect with stmp. check credentials` });
+    } else {
+      res.status(200).send();
+    }
+  });
+  if (reciever == null) {
+    // console.log(req.body)
+    res.status(401).send({ error: `Empty email address` });
+  } else {
+    return transport.sendMail(
+      {
+        from: sender,
+        to: reciever,
+        subject: "Welcome to GetBeta",
+        html: { path: "dist/welcome.html" },
+      },
+      (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("message sent");
+        }
+        transport.close();
+      }
+    );
+  }
 };
